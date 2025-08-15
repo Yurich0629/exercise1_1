b5e3387681cb8c1961ae4a8e698e4bd92c5600ab
Ilya Desyatov <appleilyadesyatov@gmail.com>
2025-08-15 10:36:17 +0300

    firstUpdate

---
+++ exercise1_1/exercise1_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise1_1
+//
+//  Created by Илья Десятов on 15.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
